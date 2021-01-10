255002c84b2274d47dd15922e049aa30a30bb319
manami-d <manami.davidson@hotmail.com>
2021-01-10 21:45:50 +1100

    Complete 89 percent

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -1,7 +1,8 @@
-function checkObj(obj, checkProp) {
-  if (obj.hasOwnProperty(checkProp)) {
-    return obj[checkProp];
-  } else {
-    return "Not Found";
+function multiplyAll(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product = product * arr [i][j];
+    }
   }
+  return product;
 }
